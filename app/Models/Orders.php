bfb785b74f7d2499c386744439262af883a38fdd
nickolss <nickolasmaiaraujo@gmail.com>
2024-08-10 14:50:44 -0300

    feat: Added orders model.

---
+++ app/Models/Orders.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    protected $table = 'orders';
+    protected $fillable = [
+        'user_id',
+        'name',
+        'number',
+        'email',
+        'method',
+        'address',
+        'total_products',
+        'total_price',
+        'placed_on',
+        'payment_status',
+        'ong'
+    ];
+}
