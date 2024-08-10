f9300fa741a7e5c89d144edb5b15d2c5a3f45740
RLC02 <codor417@gmail.com>
2024-08-09 23:45:53 -0300

    feat: Added laravel project structure

---
+++ Naturalle/resources/views/partials/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="footer section has-bg-image text-center" style="background-image: url('{{ asset('images/Footer2.jpg') }}')">
+    <div class="container">
+        <div class="footer-top grid-list">
+            <div class="footer-brand has-before has-after">
+                <a href="#" class="logo">
+                    <img src="{{ asset('images/LogoBrancaNaturalle.png') }}" width="270" height="80" loading="lazy" alt="grilli home">
+                </a>
+                <address class="body-4">
+                    Restaurante Naturalle, São Paulo 123
+                </address>
+                <a href="mailto:booking@grilli.com" class="body-4 contact-link">naturalle@gmail.com</a>
+                <a href="tel:+000000000" class="body-4 contact-link">Telefone: (11)981765432</a>
+                <p class="body-4">Funcionamento: 11:00 às 23:30</p>
+                <div class="wrapper">
+                    <div class="separator"></div>
+                    <div class="separator"></div>
+                    <div class="separator"></div>
+                </div>
+                <p class="title-1">Cadastre-se e ganhe</p>
+                <p class="label-1">Cadastre-se e Ganhe <span class="span">25% Off</span></p>
+            </div>
+            <ul class="footer-list">
+                <li><a href="index.html" class="label-2 footer-link hover-underline">Home</a></li>
+                <li><a href="#" class="label-2 footer-link hover-underline">Menu</a></li>
+                <li><a href="sobrenos.html" class="label-2 footer-link hover-underline">Sobre nós</a></li>
+                <li><a href="SaborSolidario.html" class="label-2 footer-link hover-underline">Nosso Projeto</a></li>
+                <li><a href="delivery.html" class="label-2 footer-link hover-underline">Delivery</a></li>
+            </ul>
+            <ul class="footer-list">
+                <li><a href="#" class="label-2 footer-link hover-underline">Facebook</a></li>
+                <li><a href="#" class="label-2 footer-link hover-underline">Instagram</a></li>
+                <li><a href="#" class="label-2 footer-link hover-underline">Twitter</a></li>
+                <li><a href="#" class="label-2 footer-link hover-underline">Google Maps</a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom"></div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ Naturalle/resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('content')
+    <!-- Conteúdo específico da página vai aqui -->
+@endsection
