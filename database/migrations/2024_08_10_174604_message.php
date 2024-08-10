783c5af8cda0a8e34ad282662baf33f3a7e1c5d5
nickolss <nickolasmaiaraujo@gmail.com>
2024-08-10 14:48:15 -0300

    feat: Added message migration

---
+++ database/migrations/2024_08_10_174604_message.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessageTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('number');
+            $table->text('message');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
