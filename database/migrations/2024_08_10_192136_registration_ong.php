17c62986ac00d26fd1bfafdb7a85f4d3bb8522d5
nickolss <nickolasmaiaraujo@gmail.com>
2024-08-10 16:33:26 -0300

    fix: Fixed registration_ong migration

--- database/migrations/2024_08_10_192136_registration_ong.php
+++ database/migrations/2024_08_10_192136_registration_ong.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-class CreateCadastroongTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
