467f574551450fe81a7d89e5585b26fde80c3291
nickolss <nickolasmaiaraujo@gmail.com>
2024-08-10 14:13:26 -0300

    feat: Added migration to registration_ong table

---
+++ database/migrations/2024_08_10_170914_registration_ong.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateCadastroongTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('registration_ong', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('telefone');
+            $table->string('cep');
+            $table->string('rua');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado', 2);
+            $table->string('CNPJ')->unique();
+            $table->string('tipo_ong')->nullable();
+            $table->text('observacoes')->nullable();
+            $table->timestamp('data_cadastro')->useCurrent();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('registration_ong');
+    }
+};
