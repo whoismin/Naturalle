3891e24ebde66d0021e3808e656d559f97e4612e
nickolss <nickolasmaiaraujo@gmail.com>
2024-08-10 14:51:32 -0300

    feat: Added products migration.

---
+++ database/migrations/2024_08_10_175101_products.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('category');
+            $table->text('details');
+            $table->unsignedInteger('price');
+            $table->string('image');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
