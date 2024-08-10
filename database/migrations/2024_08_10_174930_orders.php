5df43d0f2b845d234fa7c49d1916516828cf3693
nickolss <nickolasmaiaraujo@gmail.com>
2024-08-10 14:50:06 -0300

    feat: Added orders migration.

---
+++ database/migrations/2024_08_10_174930_orders.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('number');
+            $table->string('email');
+            $table->string('method');
+            $table->text('address');
+            $table->text('total_products');
+            $table->unsignedInteger('total_price');
+            $table->dateTime('placed_on');
+            $table->string('payment_status')->default('pending');
+            $table->string('ong')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
